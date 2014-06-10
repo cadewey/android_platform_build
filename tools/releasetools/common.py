9163809a25ec264a9eac48de3cb9d887f1431030
eldarerathis <eldarerathis@gmail.com>
2014-06-10 15:33:01 -0400

    Don't build recovery by default

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1160,7 +1160,7 @@ def MakeRecoveryPatch(input_dir, output_sink, recovery_img, boot_img,
 
   d = Difference(recovery_img, boot_img, diff_program=diff_program)
   _, _, patch = d.ComputePatch()
-  output_sink("recovery-from-boot.p", patch)
+  #output_sink("recovery-from-boot.p", patch)
 
   td_pair = GetTypeAndDevice("/boot", info_dict)
   if not td_pair:
@@ -1171,7 +1171,7 @@ def MakeRecoveryPatch(input_dir, output_sink, recovery_img, boot_img,
     return
   recovery_type, recovery_device = td_pair
 
-  sh = """#!/system/bin/sh
+'''  sh = """#!/system/bin/sh
 if ! applypatch -c %(recovery_type)s:%(recovery_device)s:%(recovery_size)d:%(recovery_sha1)s; then
   applypatch %(bonus_args)s %(boot_type)s:%(boot_device)s:%(boot_size)d:%(boot_sha1)s %(recovery_type)s:%(recovery_device)s %(recovery_sha1)s %(recovery_size)d %(boot_sha1)s:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
@@ -1203,4 +1203,4 @@ fi
   except (OSError, IOError), e:
     print "failed to read init.rc: %s" % (e,)
 
-  output_sink(sh_location, sh)
+  output_sink(sh_location, sh)'''
